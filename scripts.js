714cb23528b823b1fe7246005ffbb7bd870977d6
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-01 22:39:20 +0200

    Gherkin syntax outline (Feature,scenario, example)

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+`Feature: Whole Number Divider
+As a user
+I want to be able to 
+divide two numbers and get a whole number result.`
+
+`Scenario: Dividing two numbers
+Given I have entered <dividend> and <divider>
+Then I should get <result> as a whole number.`
+
+` Examples:
+  | dividend | divider | result |
+  | ---      | ---     | ---    |
+  | 20       | 10      | 2      |
+  | 20       | 3       | 6      |`
+
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
