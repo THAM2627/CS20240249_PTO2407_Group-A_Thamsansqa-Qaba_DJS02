a8d87c3e9809e0813053cbd305a37b0031865527
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-02 01:26:21 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,36 @@
 `Feature: Whole Number Divider
 As a user
 I want to be able to 
-divide two numbers and get a whole number result.`
+divide two numbers and get a whole number result.
 
-`Scenario: Dividing two numbers
+Scenario: Dividing two numbers
 Given I have entered <dividend> and <divider>
-Then I should get <result> as a whole number.`
+Then I should get <result> as a whole number.
 
-` Examples:
+ Examples:
   | dividend | divider | result |
   | ---      | ---     | ---    |
   | 20       | 10      | 2      |
-  | 20       | 3       | 6      |`
+  | 20       | 3       | 6      |
 
+  Scenario: Dividing by zero
+  Given I have entered 0 and <divider>
+  Then I should get an error message. 
+  "Unable to divide by zero" 
+  
+  Scenario: Invalid Input
+  Given I have entered <dividend> and <divider>
+  when <dividend> and <dividend> is not a number
+  Then I should get an error message.
+  "Please enter a valid number." `;
+
+  
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+
+Given: (step) => {};
+When: (step) => {};
+Then: (step) => {};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
